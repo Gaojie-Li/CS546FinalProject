e77baa3d25322243efb3e2dd4b4405cf3bf88fca
Gaojie-Li <i.am.hya.li@gmail.com>
2017-11-18 15:43:15 -0500

    Add some functions for data/list.js and data/user.js

--- data/list.js
+++ data/list.js
@@ -2,6 +2,66 @@ const mongoConnection = require("../config/mongoConnection");
 const list = mongoConnection.list;
 
 let exportedMethods = {
+    createNewList(name, authorName) {
+        return list().then((listCollection) => {
+            let newList = {
+                name: name,
+                author: authorName
+            };
+
+            return listCollection.insertOne(newList).then((newInsertInfo) => {
+                return newInsertInfo.insertedId;
+            }).then((newID) => {
+                return this.getListByID(newID);
+            });
+        });
+    },
+
+    getListByID(id) {
+        return list().then((listCollection) => {
+            return listCollection.findOne({ _id: id }).then((listInfo) => {
+                if (!listInfo) {
+                    throw "List not found";
+                }
+
+                return listInfo;
+            })
+        })
+    },
+
+    changeListName(id, newName) {
+        return this.getListByID(id).then((currentList) => {
+            currentList.name = newName;
+
+            let updateCommand = {
+                $set: currentList
+            };
+
+            return list().then((listCollection) => {
+                return listCollection.updateOne({ _id: id }, updateCommand).then(() => {
+                    return this.getListByID(id);
+                });
+            });
+        });
+    },
+
+    // check correct or not
+    addWordToList(id, word) {
+        return this.getListByID(id).then((currentList) => {
+            return list().then((listCollection) => {
+                return listCollection.updateOne({ _id: id }, {
+                    $addToSet: {
+                        words: word
+                    }
+                }).then(() => {
+                    return listCollection.findOne({ _id: id }).then((list) => {
+                        return list.words[0];
+                    });
+                });
+            });
+        });
+    }
+
 
 }
 
--- data/user.js
+++ data/user.js
@@ -3,6 +3,33 @@ const user = mongoConnection.user;
 
 let exportedMethods = {
     // All user related database access functions
+    registerNewUser(username, email, password) {
+        return user().then((userCollection) => {
+            let newUser = {
+                username: username,
+                email: email,
+                password: password
+            };
+
+            return userCollection.insertOne(newUser).then((newInsertInfo) => {
+                return newInsertInfo.insertedId;
+            }).then((newID) => {
+                return this.getUserByID(newID);
+            })
+        })
+    },
+
+    getUserByID(id) {
+        return user().then((userCollection) => {
+            return userCollection.findOne({ _id: id }).then((userProfile) => {
+                if (!userProfile) {
+                    throw "User not found";
+                }
+
+                return userProfile;
+            })
+        })
+    }
 }
 
 module.exports = exportedMethods;
