7e8885d929bff4069055c22c0509cc68316bfcd6
hli68 <hli68@stevens.edu>
2017-12-14 21:00:43 -0500

    add lists info to home/

--- routes/home.js
+++ routes/home.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const data = require("../data");
+const listsData = data.lists;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    if (!req.user){
+    if (!req.user) {
         res.render('contents/login', {
             title: 'Please Login to your account'
         });
-    }
-    else{
-        console.log(req.user);
-        res.render('contents/home', {
-            title: 'Flashcards Plus',
-            userinfo: req.user
+    } else {
+
+        listsData.getAllLists().then((lists) => {
+            console.log(lists);
+            res.render('contents/home', {
+                title: 'Flashcards Plus',
+                userinfo: req.user,
+                listsInfo: lists
+            });
+        }).catch((err) => {
+            console.log(err.message);
+            res.status(404).json({
+                error: 'List not found'
+            });
         });
+
     }
 });
 
